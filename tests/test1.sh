f2af7a1517629dac44dbeab21902df9e09b29f60
iannisimo <iannisimo@gmail.com>
2021-07-12 20:33:07 +0200

    perfect

--- tests/test1.sh
+++ tests/test1.sh
@@ -3,8 +3,6 @@ SOCKET='tests/output/test1/socket'
 
 echo "Starting clients"
 
-$CLIENT -h
-
 $CLIENT -f $SOCKET -p -t 200 -w tests/files/a\ space -t 200 -W tests/files/notSoEmptyFile,tests/files/emptyFile -t 200 -R -d tests/output/test1/client1 > tests/output/test1/client1.log 2> tests/output/test1/v_client1.log
 $CLIENT -f $SOCKET -p -t 200 -w tests/files/can -t 200 -r tests/files/can/you/search/through/folders -R -d tests/output/test1/client2 > tests/output/test1/client2.log 2> tests/output/test1/v_client2.log
 $CLIENT -f $SOCKET -p -t 200 -w tests/files,n=10 -t 200 -R n=6 -d tests/output/test1/client3 > tests/output/test1/client3.log 2> tests/output/test1/v_client3.log
